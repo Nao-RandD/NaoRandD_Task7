350884a40b395c409037d6456c6efb88ecedc6e2
Nao-RandD <nao.r.and.d@gmail.com>
2021-04-12 14:27:40 +0900

    [add] Add tabBar and calculate function for each tab.

---
+++ AppDojoTask7/AddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  AppDojoTask7
+//
+//  Created by Naoyuki Kan on 2021/04/02.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var label: UILabel!
+
+    override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+        textField1.keyboardType = .numberPad
+        textField2.keyboardType = .numberPad
+	}
+
+    @IBAction private func tappedButton(_ sender: Any) {
+        let value1 = Int(textField1.text!) ?? 0
+        let value2 = Int(textField2.text!) ?? 0
+
+        label.text = String(value1 + value2)
+    }
+}
+
